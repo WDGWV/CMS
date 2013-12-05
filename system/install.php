657cf0820881f069195b09a84f9a55879bda9348
Wesley De Groot <sales@wdgp.nl>
2013-12-05 20:32:29 +0100

    Yeey!!! SETUP WORKS NOW (DB SETUP NOT... BUT FOR LATER)

--- system/install.php
+++ system/install.php
@@ -77,6 +77,29 @@ function offlineInstall ( )
 		return false;
 }
 
+#function unichr( str ) 
+# turns int into unicode HTML
+## WdG: 5 DEC 2013
+function unichr($u) 
+{
+    return mb_convert_encoding('&#' . intval($u) . ';', 'UTF-8', 'HTML-ENTITIES');
+}
+
+#function tochr ( string(str) )
+#turns chars into code
+## WdG: 5 DEC 2013
+function tochr($char)
+{
+    $i = 0;
+    $number = '';
+    while (isset($char{$i})) 
+    {
+        $number .= ord($char{$i}) . ";";
+        ++$i;
+    }
+    return $number;
+}
+
 #function validatedInstall( file(str) )
 #checks if the file is a real WDGWV Setupfile!
 ## WdG: 30 NOV 2013
@@ -84,10 +107,10 @@ function validatedInstall ( $filesString )
 {
 	$checked = array();
 
-	if ( substr ( $filesString, 0, 15 ) == base64_decode ( "V0RHV1YgU2V0dXBGaWxlCg==" ) )
+	if ( ( substr ( $filesString, 0, 15 ) . chr(10) ) == base64_decode ( "V0RHV1YgU2V0dXBGaWxlCg==" ) )
 		$checked[] = true;
 
-	if ( substr ( $filesString, 17, 11 ) == base64_decode ( "V0RHV1YgU2V0dXAK" ) )
+	if ( ( substr ( $filesString, 17, 11 ) . chr(10) ) == base64_decode ( "V0RHV1YgU2V0dXAK" ) )
 		$checked[] = true;
 
 	if ( sizeof ( $checked ) >= 2 )
@@ -132,53 +155,86 @@ function parseSetupFiles ( $filesString )
 
 	if ( validatedInstall ( $filesString ) )
 	{
-		$filesString = preg_replace("#\r#", null, $filesString);
-		$explodeAll  = explode("\n", $filesString);
+		$filesString  = preg_replace("#\r#", null, $filesString);
+		$explodeAll   = explode("\n", $filesString);
+		$setupRunning = true;
+		if ( !is_writable('.') )
+		{
+			$setupLog['error'][] = 'Not writeable file system';
+			echo "Not writeable file system!";
+			setupWrite('setup.log', $setupLog);
+			$setupRunning = false;
+		}
 
 		for ( $i=0; $i<sizeof($explodeAll); $i++ )
 		{
-			if ( substr ( $explodeAll[$i], 0, 2 ) == "~~" )
+			if ( $setupRunning )
 			{
-				if ( @mkdir ( substr ( $explodeAll[$i], 2 ) ) )
+				if ( substr ( $explodeAll[$i], 0, 2 ) == "~~" )
 				{
-					$setupLog['info'][] = 'Dir "' . substr ( $explodeAll[$i], 0, 1 ) . '" Created';
+					if ( !is_dir ( substr ( $explodeAll[$i], 2 ) ) )
+					{
+						if ( @mkdir ( substr ( $explodeAll[$i], 2 ) ) )
+						{
+							$setupLog['info'][] = 'Dir "' . substr ( $explodeAll[$i], 2 ) . '" Created';
+						}
+						else
+						{
+							$setupLog['error'][] = 'Dir "' . substr ( $explodeAll[$i], 2 ) . '" not Created';
+							setupWrite('setup.log', $setupLog);
+							$setupRunning = false;
+							return false;
+							break;
+						}
+					}
+					else
+					{
+						$setupLog['info'][] = 'Dir "' . substr ( $explodeAll[$i], 2 ) . '" already created.';
+					}
+				}
+				elseif ( substr ( $explodeAll[$i], 0, 1 ) == "~" && substr ( $explodeAll[$i], 0,2 ) != "~~" )
+				{
+					// PUT TO DB
+					#TODO
 				}
 				else
 				{
-					$setupLog['error'][] = 'Dir "' . substr ( $explodeAll[$i], 0, 1 ) . '" not Created';
-					echo "Setup Failed, see Error log!";
-					setupWrite('setup.log', $setupLog);
-					break;
-				}
-			}
-			elseif ( substr ( $explodeAll[$i], 0, 1 ) == "~" && substr ( $explodeAll[$i], 0,2 ) != "~~" )
-			{
-				// PUT TO DB
-				#TODO
-			}
-			else
-			{
-				$file  = explode("~", $explodeAll[$i]);
-				$file1 = setupWrite ( base64_decode ( $file[0] ), 	 		   base64_decode ( $file[2] ) );
-				$file2 = setupWrite ( base64_decode ( $file[0] ) . '.version', base64_decode ( $file[1] ) );
+					$file  = explode("~", $explodeAll[$i]);
 
-				if ( $file1 && $file2 )
-				{
-					$setupLog['info'][] = 'File "' . $file[0] . '" Created';
-				}
-				else
-				{
-					$setupLog['error'][] = 'File "' . $file[0] . '" not Created';
-					echo "Setup Failed, see Error log!";
-					setupWrite('setup.log', $setupLog);
+					if ( sizeof($file) == 3 )
+					{
+						$setupLog['x-files'][base64_decode ( $file[0] )] = $file;
+
+						$file1 = setupWrite ( base64_decode ( $file[0] ), 	 		   base64_decode ( $file[2] ) );
+						$file2 = setupWrite ( base64_decode ( $file[0] ) . '.version', base64_decode ( $file[1] ) );
+
+						if ( $file1 && $file2 )
+						{
+							$setupLog['info'][] = 'File "' . base64_decode ( $file[0] ) . '" Created';
+						}
+						else
+						{
+							$setupLog['error'][] = 'File "' . base64_decode ( $file[0] ) . '" not Created';
+							$setupRunning = false;
+							setupWrite('setup.log', $setupLog);
+							return false;
+							break;
+						}
+					}
 				}
 			}
 		}
 	}
 	else
 	{
-		echo "The setupfile seems to be corrupted, the setup cannot continue!";
+		$setupLog['error'][] = 'Setupfile Corrupted!!!';
+		setupWrite('setup.log', "The setupfile seems to be corrupted, the setup cannot continue!");
+		return false;
 	}
+
+	#IF DEBUG VERSION THEN..
+	setupWrite('setup.log', $setupLog);
+	return true;
 }
 
 #function beginOfflineInstal()
@@ -188,7 +244,14 @@ function beginOfflineInstall ( )
 {
 	if ( is_readable ( 'install' ) )
 	{
-		parseSetupFiles ( file_get_contents ( 'install' ) );
+		if ( parseSetupFiles ( file_get_contents ( 'install' ) ) ) 
+		{
+			echo "Installed!!!";
+		}
+		else
+		{
+			echo file_get_contents('setup.log');
+		}
 	}
 }
 
