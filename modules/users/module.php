48b1985975942942c469a5674e72b0b2d14d2fe1
Wesley De Groot <sales@wdgp.nl>
2014-01-06 20:29:28 +0100

    Try to add some module support :p

--- modules/users/module.php
+++ modules/users/module.php
@@ -54,17 +54,46 @@
   All Rights Reserved.
 */
 
-if ( defined ( 'WDGWVSystem' ) )
-{
-	if ( !installed ( 'iframe' ) )
-		WDGWV_add_page('iframe', '{IFRAME SRC="http://www.wdgwv.com" WIDTH="" HEIGHT=""}');
+//Add some pages (and with contents 'hook from module')
+//can be STATIC or INCLUDE (for include use: add_page('pagename', 'module/pagename.php'); )
+add_page('user/login',		'hook from module');
+add_page('user/register',	'hook from module');
+add_page('user/profile',	'hook from module');
+add_page('user/posts',		'hook from module');
 
-	WDGWV_add_replacer('IFRAME', 'This is the agenda');
-	WDGWV_custom_replacer(
-							'IFRAME',
-							array('SRC', 'WIDTH', 'HEIGHT'),
-							array(null,  '100%',  '100%')
-						  );
-	//WDGWV_add_admin('iframe', mod_adminIframe())
+// Add if they want a sidebar hook!!!
+hook('sidebar',				login_mini());
+
+// are you logged in?
+define('LOGGEDIN', 			login_checklogin());
+
+#function login_checklogin()
+#checks if the user is logged in or not.
+## WdG: 06 JAN 2014
+function login_checklogin ( )
+{
+	if ( isset ( $_SESSION['uid'] ) )
+	{
+		if ( $_SESSION['ip'] == $_SERVER['REMOTE_ADDR'] )
+		{
+			return true;
+		}
+	}
+
+	/*
+	 Not logged in / session hijacked
+	*/
+	return false;
+}
+
+#function login_mini()
+#Display's a mini login.
+## WdG: 30 NOV 2013
+function login_mini ( )
+{
+	if ( !login_checklogin() )
+		return "LOGIN MINI SIDEBAR ITEM";
+	else
+		return "LOGGED IN SIDEBAR ITEM";
 }
 ?>
--- modules/users/profile.php
+++ modules/users/profile.php
@@ -54,56 +54,8 @@
   All Rights Reserved.
 */
 
-class ical
- {
-  function __construct($DEC)
-    {
-      $this->cal = "BEGIN:VCALENDAR\n" .
-                   "PRODID:-//WDGWV\v1.0\n" .
-                   "VERSION:2.0\n" .
-                   "CALSCALE:GREGORIAN\n" .
-                   "METHOD:PUBLISH\n" .
-                   "X-WR-CALNAME: {$DEC}\n";
-    }
+include_once 'includes/load.php';
+//include_once 'includes/core.php';
 
-  //TODO extract($array)
-  function addevent($title,$desc,$date=null,$time=null, $dend=null)
-    {
-      //extract($array);
-    	
-      if ( $date == null )
-        $date = date("Ymd");
-
-      if ( $dend == null )
-        $dend = date("Ymd",(time()+((60*60)*24)));
-        
-      if ( $time == null )
-        $time = date("His");
-        
-      $this->cal .= "BEGIN:VEVENT\n" .
-                    "DTSTART;VALUE=DATE:" . $date . "\n" .
-                    "DTEND;VALUE=DATE:"   . $dend . "\n" .
-                    "URL:http://www.wdgss.nl\n" .
-                    "LOCATION:wdgss.nl\n" .
-                    "SUMMARY:{$title}\n" .
-                    "DESCRIPTION:{$desc}\n" .
-                    "CLASS:PUBLIC\n" .
-                    "TRANSP:TRANSPARENT\n" .
-                    "STATUS:CONFIRMED\n" .
-                    "DTSTAMP:" . $date ."T" . $time . "\n" .
-                    "CATEGORIES:WDGSS\n" .
-                    "UID:" . uniqueid() . "@wdgss.nl\n" .
-                    "END:VEVENT\n";
-
-    }
-
-  function send()
-    {
-      $this->cal .= "END:VCALENDAR";
-        header("Content-Type: text/Calendar");
-        header("Content-Disposition: inline; filename=calendar.ics");
-        echo $this->cal;
-    }
- }
 
 ?>
