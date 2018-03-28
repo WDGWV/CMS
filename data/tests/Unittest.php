a2196288c188d9389fc3dc937f764780d9113996
Wesley de Groot <wes@vista.aero>
2018-03-28 17:28:06 +0200

    updated tests

--- data/tests/Unittest.php
+++ data/tests/Unittest.php
@@ -5,7 +5,7 @@
 
 final class test extends TestCase {
 	public function setUp() {
-		echo sprintf("Testing '%s'", $this->getName());
+		echo sprintf("Testing '%s'.%s", $this->getName(), PHP_EOL);
 	}
 
 	public function testWillPass() {
