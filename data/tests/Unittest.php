a0b4f3d8407030eb63210de5e97cb12887a1a0c8
Wesley de Groot <wes@vista.aero>
2018-03-28 17:27:25 +0200

    updated tests

--- data/tests/Unittest.php
+++ data/tests/Unittest.php
@@ -4,6 +4,10 @@
 /**/
 
 final class test extends TestCase {
+	public function setUp() {
+		echo sprintf("Testing '%s'", $this->getName());
+	}
+
 	public function testWillPass() {
 		$this->assertEquals('a', 'a');
 	}
