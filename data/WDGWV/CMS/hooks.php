22c8f8674ac86cd8df448787cd3f16b814a405f8
Wesley de Groot <wes@vista.aero>
2018-04-06 23:33:27 +0200

    Maximized extendability

--- data/WDGWV/CMS/hooks.php
+++ data/WDGWV/CMS/hooks.php
@@ -74,50 +74,121 @@ class hooks extends \WDGWV\CMS\baseProtected {
 	}
 
 	public function loopHooks($which) {
-		if (is_array($which)) {
-			for ($i = 0; $i < sizeof($which); $i++) {
-				$this->loopHook($which[$i]);
-			}
+		if (!is_array($which)) {
+			$which = array($which);
+		}
+
+		for ($i = 0; $i < sizeof($which); $i++) {
+			$this->loopHook($which[$i]);
 		}
 	}
 
 	public function haveHooksFor($which) {
-		if (is_array($which)) {
-			for ($i = 0; $i < sizeof($which); $i++) {
-				if (sizeof($this->loopHook($which[$i])) > 0) {
-					return true;
-				}
+		if (!is_array($which)) {
+			$which = array($which);
+		}
+
+		for ($i = 0; $i < sizeof($which); $i++) {
+			if (sizeof($this->loopHook($which[$i])) > 0) {
+				return true;
 			}
 		}
 	}
 
+	public function loadHooksFor($which) {
+		if (!is_array($which)) {
+			$which = array($which);
+		}
+
+		return $this->loadPageFor($which);
+	}
+
 	public function loadPageFor($which) {
-		if (is_array($which)) {
-			for ($i = 0; $i < sizeof($which); $i++) {
-				if (sizeof(($x = $this->loopHook($which[$i]))) > 0) {
-					return $x;
-				}
+		if (!is_array($which)) {
+			$which = array($which);
+		}
+
+		for ($i = 0; $i < sizeof($which); $i++) {
+			if (sizeof(($x = $this->loopHook($which[$i]))) > 0) {
+				return $x;
 			}
 		}
 	}
 
 	public function loopHook($at) {
 		switch ($at) {
+		case 'before-content':
+			if (isset($this->hookDatabase['before-content'])) {
+				for ($i = 0; $i < sizeof($this->hookDatabase['before-content']); $i++) {
+					return $this->hookDatabase['before-content'][$i]['action'];
+				}
+			}
+			break;
+
+		case 'after-content':
+			if (isset($this->hookDatabase['after-content'])) {
+				for ($i = 0; $i < sizeof($this->hookDatabase['after-content']); $i++) {
+					return $this->hookDatabase['after-content'][$i]['action'];
+				}
+			}
+			break;
+
 		case 'url':
 			if (isset($this->hookDatabase['url'])) {
 				for ($i = 0; $i < sizeof($this->hookDatabase['url']); $i++) {
+					if (!isset($this->hookDatabase['url'][$i]['name'])) {
+						continue;
+					}
+
 					$safeMatch = $this->hookDatabase['url'][$i]['name'];
 					$safeMatch = preg_replace("/\//", "\\\\/", $safeMatch);
 					$safeMatch = preg_replace("/\*/", "(.*)", $safeMatch);
 					if (isset($_SERVER['REQUEST_URI'])) {
-						if (preg_match("/" . $safeMatch . "$/", $_SERVER['REQUEST_URI'])) {
+						$niceURL = $_SERVER['REQUEST_URI'];
+
+						// if the URL have a "?" then get only before the "?"
+						if (sizeof(explode('?', $niceURL)) > 0) {
+							$niceURL = explode('?', $niceURL)[0];
+						}
+
+						if (preg_match("/" . $safeMatch . "$/", $niceURL)) {
 							if (is_callable($this->hookDatabase['url'][$i]['action'])) {
-								return call_user_func_array(
+								$returnValue = call_user_func_array(
 									$this->hookDatabase['url'][$i]['action'],
 									$this->hookDatabase['url'][$i]['params']
 								);
+
+								if (!$returnValue) {
+									// Temporary unset this hook, it's already loaded.
+									unset($this->hookDatabase['url'][$i]);
+
+									// Continue testing
+									continue;
+								}
+
+								return $returnValue;
 							} else {
-								echo sprintf('"%s" is not a function!', $this->hookDatabase['url'][$i]['action'][1]);
+								if (sizeof($this->hookDatabase['url'][$i]['action']) > 1) {
+									Debugger::sharedInstance()->error(sprintf(
+										'replacer [%s]: "(new \%s)->%s(%s)" is not callable.',
+
+										$this->hookDatabase['url'][$i]['name'],
+
+										get_class($this->hookDatabase['url'][$i]['action'][0]),
+										$this->hookDatabase['url'][$i]['action'][1],
+
+										(sizeof($this->hookDatabase['url'][$i]['params']) > 0
+											? implode(', ', $this->hookDatabase['url'][$i]['params'])
+											: ''
+										)
+									));
+								} else {
+									Debugger::sharedInstance()->error(sprintf('replacer [%s]: "%s" is not callable',
+										$this->hookDatabase['url'][$i]['name'],
+										$this->hookDatabase['url'][$i]['action']
+									));
+								}
+
 							}
 						}
 					}
@@ -130,10 +201,16 @@ class hooks extends \WDGWV\CMS\baseProtected {
 				for ($i = 0; $i < sizeof($this->hookDatabase['get']); $i++) {
 					if (isset($_GET[$this->hookDatabase['get'][$i]['name']])) {
 						if (is_callable($this->hookDatabase['get'][$i]['action'])) {
-							return call_user_func_array(
+							$returnValue = call_user_func_array(
 								$this->hookDatabase['get'][$i]['action'],
 								$this->hookDatabase['get'][$i]['params']
 							);
+
+							if (!$returnValue) {
+								continue;
+							}
+
+							return $returnValue;
 						} else {
 							echo sprintf('"%s" is not a function!', $this->hookDatabase['get'][$i]['action'][1]);
 						}
@@ -147,10 +224,16 @@ class hooks extends \WDGWV\CMS\baseProtected {
 				for ($i = 0; $i < sizeof($this->hookDatabase['post']); $i++) {
 					if (isset($_POST[$this->hookDatabase['post'][$i]['name']])) {
 						if (is_callable($this->hookDatabase['post'][$i]['action'])) {
-							return call_user_func_array(
+							$returnValue = call_user_func_array(
 								$this->hookDatabase['post'][$i]['action'],
 								$this->hookDatabase['post'][$i]['params']
 							);
+
+							if (!$returnValue) {
+								continue;
+							}
+
+							return $returnValue;
 						} else {
 							echo sprintf('"%s" is not a function!', $this->hookDatabase['post'][$i]['action'][1]);
 						}
@@ -176,6 +259,10 @@ class hooks extends \WDGWV\CMS\baseProtected {
 	}
 
 	public function createHook($at, $name, $action, $params = array()) {
+		if (!isset($this->hookDatabase[$at])) {
+			$this->hookDatabase[$at] = array();
+		}
+
 		$this->hookDatabase[$at][] = array(
 			'name' => $name,
 			'action' => $action,
--- data/extensions/todo/extension.php
+++ data/extensions/todo/extension.php
@@ -90,38 +90,50 @@ class todoExtension extends \WDGWV\CMS\extensionBase {
 				<ul>
 					<li>*<progress min=0 max=100 value=100></progress> 100% | Connection (N/A)</li>
 					<li>*<progress min=0 max=100 value=100></progress> 000% | ... Other</li>
-				</ul><br />Overall progress: <progress min=0 max=2 value=2></progress> 2/2<br /><br />'),
+				</ul><br />Overall progress: <progress min=0 max=2 value=2></progress> 2/2<br /><br />', ),
 
-			array('Extensibility', "---<br />Extensibility for plugins<br /><br />
+			array('Extensibility', '---<br />Extensibility for extensions/plugins/modules<br /><br />
 				<b>**Supported**</b><br /><br />
 				<ul>
 					<li>*<progress min=0 max=100 value=100></progress> 100% | Page extensions</li>
 					<li>*<progress min=0 max=100 value=100></progress> 100% | Menu extensions</li>
 					<li>*<progress min=0 max=100 value=100></progress> 100% | URL-extensions (override)</li>
-					<li>*<progress min=0 max=100 value=100></progress> 100% | Specific \$_POST extensions</li>
-					<li>*<progress min=0 max=100 value=100></progress> 100% | Specific \$_GET extensions</li>
+					<li>*<progress min=0 max=100 value=100></progress> 100% | before-content (one item only)</li>
+					<li>*<progress min=0 max=100 value=100></progress> 100% | after-content (one item only)</li>
+					<li>*<progress min=0 max=100 value=100></progress> 100% | Specific $_POST extensions</li>
+					<li>*<progress min=0 max=100 value=100></progress> 100% | Specific $_GET extensions</li>
 					<li>*<progress min=0 max=100 value=25></progress> 025% | Partial: UBB code extensions</li>
 				</ul><br />
-				Progress: <progress min=0 max=6 value=5></progress> 5/6<br />", ),
+				Progress: <progress min=0 max=6 value=5></progress> 7/8<br />'),
+
+			array('Extension: demo mode', '---<br />Extension: demo mode<br /><br />
+				<b>**Supported**</b><br /><br />
+				<ul>
+					<li>*<progress min=0 max=100 value=100></progress> 100% | Strip $_POST</li>
+					<li>*<progress min=0 max=100 value=100></progress> 100% | Strip $_GET</li>
+					<li>*<progress min=0 max=100 value=100></progress> 100% | Warning if $_GET or $_POST (before-content)</li>
+					<li>*<progress min=0 max=100 value=100></progress> 100% | Extra footer notice (after-content)</li>
+				</ul><br />Overall progress: <progress min=0 max=4 value=4></progress> 4/4<br /><br />', ),
+
 			array('item 2', '---<br />description<br /><br />
 				<b>**Supported**</b><br /><br />
 				<ul>
 					<li>*<progress min=0 max=100 value=0></progress> 000% | ...</li>
 					<li>*<progress min=0 max=100 value=0></progress> 000% | ...</li>
-				</ul><br />Overall progress: <progress min=0 max=2 value=0></progress> 0/2<br /><br />'),
+				</ul><br />Overall progress: <progress min=0 max=2 value=0></progress> 0/2<br /><br />', ),
 
 			array('MySQL Database support', '---<br />MySQL Database support<br /><br />
 				<b>**Supported**</b><br /><br />
 				<ul>
 					<li>*<progress min=0 max=100 value=0></progress> 000% | Connection</li>
 					<li>*<progress min=0 max=100 value=0></progress> 000% | ...</li>
-				</ul><br />Overall progress: <progress min=0 max=2 value=0></progress> 0/2<br /><br />'),
+				</ul><br />Overall progress: <progress min=0 max=2 value=0></progress> 0/2<br /><br />', ),
 			array('SQLite Database support', '---<br />SQLite Database support<br /><br />
 				<b>**Supported**</b><br /><br />
 				<ul>
 					<li>*<progress min=0 max=100 value=0></progress> 000% | Connection</li>
 					<li>*<progress min=0 max=100 value=0></progress> 000% | ...</li>
-				</ul><br />Overall progress: <progress min=0 max=2 value=0></progress> 0/2<br /><br />'),
+				</ul><br />Overall progress: <progress min=0 max=2 value=0></progress> 0/2<br /><br />', ),
 
 			array('MAYBE LATER: CRM',
 				'---<br />CRM Support (extension?)<br /><br />Depends on: usage of CMS<br /><br />Progress: <progress min=0 max=100 value=0></progress> 0%<br /><br />'),
